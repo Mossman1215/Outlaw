4ff12e844f8ada7ccf618345224c5424f58b927b
Moss Cantwell <mossman2142@gmail.com>
2019-03-19 07:19:47 +1300

    test version

--- readfrisp.go
+++ readfrisp.go
@@ -1,67 +1,97 @@
 package main
+
 /*
 *	"crypto/rsa"
 *	"github.com/aws/aws-sdk-go/aws"
-*/
+ */
 import (
-	"fmt"
-    "github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
 	"context"
-	"github.com/aws/aws-lambda-go/lambda"
+	"encoding/json"
+	"fmt"
 	"os"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
+
 /**
-* env is path in the s3 bucket or what frisp calls a vault
-*/
-type Event struct {
-	bucket string `json:"bucket"`
-	env string `json:"environment"`
-	region string `json:"region"`
-	filename string `json:"filename"`
+* Event contains input data for the lambda
+* env is folder in the s3 bucket or what frisp calls a vault
+ */
+type event struct {
+	Bucket   string `json:"bucket"`
+	Env      string `json:"environment"`
+	Region   string `json:"region"`
+	Filename string `json:"filename"`
 }
 
-type KeyResponse struct {
-	ciper string `json:"cipher:"`
-	plain string `json:"plain:"`
+type keyResponse struct {
+	Ciper string `json:"cipher:"`
+	Plain string `json:"plain:"`
 	//key,cert,status?
 }
 
 func exitErrorf(msg string, args ...interface{}) {
-    fmt.Fprintf(os.Stderr, msg+"\n", args...)
-    os.Exit(1)
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
 }
+
 /*
  encrypt data with a public key (defaults to RSAES PKCS#1 v1.5)
 var encrypted = publicKey.encrypt(bytes);
 */
-func HandleRequest(ctx context.Context, config Event) (string, error) {
+func HandleRequest(ctx context.Context, config event) (string, error) {
 	/*
 	* get frisp privatekey from s3
 	* attempt to read environment encrypted data (SSL cert and key)
 	* use frisp private key to decrypt environment SSL cert and key
 	* print/dump result to caller as json
-	*/
-	sess := session.Must(session.NewSession())
+	 */
+	bucket := config.Bucket
+	env := config.Env
+	//filename := config.Filename
+	item := "_private/" + env + ".enc"
+	sess, _ := session.NewSession(&aws.Config{
+		Region: aws.String(config.Region)},
+	)
 
-	svc := s3.New(sess)
-
-	i := 0
-	err := svc.ListObjectsPages(&s3.ListObjectsInput{
-		Bucket: &config.region,
-	}, func(p *s3.ListObjectsOutput, last bool) (shouldContinue bool) {
-		fmt.Println("Page,", i)
-		i++
-
-		for _, obj := range p.Contents {
-			fmt.Println("Object:", *obj.Key)
-		}
-		return true
-	})
-	if err != nil {
-		return "failed to list objects", err
+	downloader := s3manager.NewDownloader(sess)
+	encValue := aws.NewWriteAtBuffer([]byte{})
+	requestInput := &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(item),
 	}
-	return fmt.Sprintf("Hello %s!", config.region ), nil
+	numBytes, err := downloader.Download(encValue,
+		requestInput)
+	if err != nil {
+		exitErrorf("Unable to download item %q, %v", item, err)
+	}
+
+	fmt.Println("Downloaded", len(encValue.Bytes()), numBytes, "bytes")
+	encryptedStr := fmt.Sprintf("%q", encValue.Bytes())
+
+	//read private key into memory
+	//privKey := aws.NewWriteAtBuffer([]byte{})
+	//privKeyRequestInput := &s3.GetObjectInput{
+	//	Bucket: aws.String(bucket),
+	//	Key:    aws.String("_private/" + env + ".inc"),
+	//}
+	//numBytespriv, err := downloader.Download(privKey,
+	//	privKeyRequestInput)
+	//if err != nil {
+	//	exitErrorf("Unable to download item %q, %v", item, err)
+	//}
+	//create response struct and assign existing encrypted key to it
+
+	response := &keyResponse{encryptedStr, ""}
+	responsestr, err := json.Marshal(response)
+	if err != nil {
+		exitErrorf("failure to encode json", err)
+	}
+	return string(responsestr), nil
 }
 
 func main() {
---
+++ release.sh
@@ -0,0 +1,2 @@
+zip readfrisp.zip readfrisp
+aws lambda update-function-code --function-name arn:aws:lambda:us-west-2:786994140874:function:readfrisp --zip-file fileb://readfrisp.zip
